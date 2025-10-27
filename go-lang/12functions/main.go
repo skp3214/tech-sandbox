d6fbdbf8e556ebf328dfad81bd1903692294ebc4
Sachin Prajapati <skprajapati3214@gmail.com>
2025-10-27 10:26:32 +0530

    all about functions

---
+++ go-lang/12functions/main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Create a function
+
+		func functionName(){
+
+		}
+
+		call a function
+		functionName();
+
+		Parameter and arguments
+
+		func functionName(param1 type, param2 type, param3 type){
+
+		}
+
+		Return Types
+
+		func functionName(param1 type, param2 type, param3 type) type {
+
+		}
+
+		Named Return Values
+
+		func functionName(param1 type, param2 type, param3 type) (variableName type) {
+
+		}
+
+		Multi return values
+
+		func functionName(param1 type, param2 type, param3 type) (variableName1 type, variableName2 type) {
+
+		}
+
+		Recursion Functions
+
+
+	*/
+
+	familyName("Sachin")
+	myFunction(5, 6)
+	addTwoNum(6, 7)
+
+	_, b := returnTwoValues(6, "Hello ")
+	fmt.Println(b)
+
+	result:=factorial_recursion(5)
+	fmt.Println(result)
+}
+
+func familyName(name string) {
+	fmt.Println("Your name is: ", name)
+}
+
+func myFunction(x int, y int) int {
+	return x + y
+}
+
+func addTwoNum(x int, y int) (result int) {
+	result = x + y
+	return result
+}
+
+func returnTwoValues(x int, y string) (result int, text1 string) {
+	result = x + 5
+	text1 = y + "World!"
+	return
+}
+
+func factorial_recursion(x int) (y int) {
+	if x > 0 {
+		y = x * factorial_recursion(x-1)
+	}else{
+		y=1
+	}
+	return
+}
