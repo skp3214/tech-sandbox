2814048875b97de9d91013b872b2435415a8649b
Sachin Prajapati <skprajapati3214@gmail.com>
2025-11-03 22:55:02 +0530

    how to make POST request in golang

--- go-lang/22webrequests/main.go
+++ go-lang/22webrequests/main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	fmt.Println("Welcome to web request in golang")
-	performGET()
+	// performGET()
+	performPost()
 }
 
 func performGET() {
@@ -30,7 +31,33 @@ func performGET() {
 	var responseString strings.Builder
 	contentnew, _ := io.ReadAll(response.Body)
 	byteCount, _ := responseString.Write(contentnew)
-	fmt.Println("ByteCount is: ",byteCount)
+	fmt.Println("ByteCount is: ", byteCount)
 	fmt.Println(responseString.String())
 
+}
+
+func performPost() {
+	const myurl = "http://localhost:3000/post"
+
+	// fake json payload
+
+	requestBody := strings.NewReader(`
+	{
+	  "coursename":"Let's go with golang",
+	  "price":0,
+	  "platform":"learnCodeOnline.in"
+	}
+	`)
+
+	response,err:=http.Post(myurl,"application/json",requestBody)
+
+	if err!=nil{
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content,_:=io.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
