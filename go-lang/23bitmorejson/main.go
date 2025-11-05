e34de29b1ff0214e686e1595d7eaf82101bb77cf
Sachin Prajapati <skprajapati3214@gmail.com>
2025-11-05 21:28:03 +0530

    Refactor main function to call DecodeJSON instead of EncodeJSON

--- go-lang/23bitmorejson/main.go
+++ go-lang/23bitmorejson/main.go
@@ -15,7 +15,7 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to JSON video")
-	EncodeJSON()
+	DecodeJSON()
 }
 
 func EncodeJSON() {
@@ -36,4 +36,36 @@ func EncodeJSON() {
 	}
 
 	fmt.Printf("%s\n", finalJson)
+}
+
+func DecodeJSON() {
+	jsonDataFromWeb := []byte(`
+	{
+        "coursename": "JavaScriptJS Bootcamp",
+        "Price": 299,
+        "website": "LearnCodeOnline.in",
+        "tags": ["google","js"]
+        }
+	`)
+
+	var lcoCourse course
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+
+	// some cases where you just want to add data to key value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k,v:=range myOnlineData{
+		fmt.Printf("Key is %v and value is %v and Type is:%T\n",k,v,v)
+	}
 }
