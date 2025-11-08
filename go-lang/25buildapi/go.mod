e46489251e3d8d5ca7eca67434579aae77ab39f8
Sachin Prajapati <skprajapati3214@gmail.com>
2025-11-08 23:25:19 +0530

    Add initial implementation of Go API with course and author models

---
+++ go-lang/25buildapi/go.mod
@@ -0,0 +1,5 @@
+module github.com/skp3214
+
+go 1.25.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ go-lang/25buildapi/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// model for course - file
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fakeDB
+
+var courses []Course
+
+// middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+	fmt.Println("Welcome to building api in golang")
+
+}
+
+// controller
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1> Welcome to API by skp3214"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get All Courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through course, find matching id and return the response
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No course found with given id")
+}
