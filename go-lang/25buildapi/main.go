ea5d440d4523626aaeb37cb7c166d01d7b5da755
Sachin Prajapati <skprajapati3214@gmail.com>
2025-11-10 23:05:45 +0530

    Add routing and logging for course API endpoints

--- go-lang/25buildapi/main.go
+++ go-lang/25buildapi/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // model for course - file
@@ -36,6 +38,57 @@ func (c *Course) IsEmpty() bool {
 func main() {
 	fmt.Println("Welcome to building api in golang")
 
+	r := mux.NewRouter()
+
+	courses = append(courses, Course{
+		CourseId:    "2",
+		CourseName:  "ReactJS",
+		CoursePrice: 299,
+		Author: &Author{
+			Fullname: "Sachin",
+			Website:  "lco.dev",
+		},
+	})
+	courses = append(courses, Course{
+		CourseId:    "3",
+		CourseName:  "NextJS",
+		CoursePrice: 399,
+		Author: &Author{
+			Fullname: "Sameer",
+			Website:  "lco.dev",
+		},
+	})
+	courses = append(courses, Course{
+		CourseId:    "4",
+		CourseName:  "ReactNative",
+		CoursePrice: 499,
+		Author: &Author{
+			Fullname: "Sam",
+			Website:  "lco.dev",
+		},
+	})
+	courses = append(courses, Course{
+		CourseId:    "5",
+		CourseName:  "AngularJS",
+		CoursePrice: 599,
+		Author: &Author{
+			Fullname: "Sachin",
+			Website:  "lco.dev",
+		},
+	})
+
+	// routing
+	r.HandleFunc("/",serveHome).Methods("GET")
+	r.HandleFunc("/courses",getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}",getOneCourse).Methods("GET")
+	r.HandleFunc("/course",createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}",updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}",deleteOneCourse).Methods("DELETE")
+	r.HandleFunc("/",serveHome).Methods("GET")
+
+	// listen to a port
+
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controller
@@ -79,6 +132,7 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	if r.Body == nil {
 		json.NewEncoder(w).Encode("Please send some data")
+		return
 	}
 
 	// what about data = {}
@@ -88,6 +142,7 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	if course.IsEmpty() {
 		json.NewEncoder(w).Encode("No data inside {}")
+		return
 	}
 
 	// generate a unique id, string
@@ -97,8 +152,6 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	course.CourseId = strconv.Itoa(rand.Intn(100))
 	courses = append(courses, course)
 	json.NewEncoder(w).Encode(course)
-
-	return
 
 }
 
