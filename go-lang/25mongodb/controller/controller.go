b85f6bf9fec81309e06f7fd00c0b778f3cb19501
Sachin Prajapati <skprajapati3214@gmail.com>
2025-11-13 22:57:41 +0530

    Implement MongoDB insert and update functions in controller

--- go-lang/25mongodb/controller/controller.go
+++ go-lang/25mongodb/controller/controller.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"mongoskp3214/model"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -21,13 +24,39 @@ func init() {
 
 	// connect t mongodb
 	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err!=nil{
+	if err != nil {
 		log.Fatal("MongoDB connection success")
 	}
-	fmt.Println ("MongoDB connection success")
+	fmt.Println("MongoDB connection success")
 
 	collection = client.Database(dbName).Collection(colName)
 
 	fmt.Println("Collection instance is ready")
 }
 
+// MONGODB helpers - file
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie in db with id: ", inserted.InsertedID)
+}
+
+func updateOneMovie(movieID string) {
+	id, err := primitive.ObjectIDFromHex(movieID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count: ", result.ModifiedCount)
+}
