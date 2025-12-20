f279c3bb71da5bd48b1b40b0ec1ab0e7904f7d7d
Sachin Prajapati <skprajapati3214@gmail.com>
2025-12-20 14:14:31 +0530

    🍃 Spring Boot Speed Run ✅

---
+++ spring-boot/learnspringboot/src/main/java/com/example/learnspringboot/LearnspringbootApplication.java
@@ -0,0 +1,33 @@
+package com.example.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
+@SpringBootApplication
+public class LearnspringbootApplication {
+
+	public static void main(String[] args) {
+		java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("UTC"));
+		SpringApplication.run(LearnspringbootApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> myRoutes(CustomerRepository repository){
+		return route()
+				.GET("/customers", request -> ok().body(repository.findAll()))
+				.build();
+	}
+}
+
+record Customer(int id, String name){
+}
+interface CustomerRepository extends ListCrudRepository<Customer,Integer>{
+
+}
---
+++ spring-boot/learnspringboot/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into customer(name) values ('Sachin');
+insert into customer(name) values ('Sameer');
+insert into customer(name) values ('Sam');
+insert into customer(name) values ('Samrat');
+insert into customer(name) values ('Shaswat');
